d005852ca1bc22008111b21944f28741a20478b1
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2017-02-06 20:09:22 +0100

    Add ratcave examples

---
+++ ratcave/tut1.py
@@ -0,0 +1,26 @@
+import pyglet
+import ratcave as rc
+
+# Create Window
+window = pyglet.window.Window()
+
+def update(dt):
+    pass
+pyglet.clock.schedule(update)
+
+# Insert filename into WavefrontReader.
+obj_filename = rc.resources.obj_primitives
+obj_reader = rc.WavefrontReader(obj_filename)
+
+# Create Mesh
+monkey = obj_reader.get_mesh("Monkey")
+monkey.position = 0, 0, -2
+
+# Create Scene
+scene = rc.Scene(meshes=[monkey])
+
+@window.event
+def on_draw():
+    scene.draw()
+
+pyglet.app.run()
