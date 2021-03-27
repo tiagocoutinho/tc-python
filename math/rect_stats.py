6e473f688b4a64c08692317955922ab1cb1ac704
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2021-03-27 19:57:06 +0100

    Make it work with any int type

--- math/rect_stats.py
+++ math/rect_stats.py
@@ -1,24 +1,25 @@
+import sys
 import math
 import collections
 
 import numba
 import numpy
 
-
-MIN_I32 = -2**31
-MAX_I32 = 2**31 - 1
-
+IMAX = sys.maxsize
+IMIN = -IMAX - 1
 
 Stats = collections.namedtuple("Stats", "min max sum mean std")
 
 
 def stats(data):
-    return Stats(data.min(), data.max(), data.sum(), data.mean(), data.std())
+    return Stats(
+        data.min(), data.max(), data.sum(), data.mean(), data.std(),
+    )
 
 
-def py_stats_i32(data):
+def py_stats_int(data):
     # with proper std
-    m, M, total, var, N = MAX_I32, MIN_I32, 0, 0, data.size
+    m, M, total, var, N = IMAX, IMIN, 0, 0, data.size
     for row in data:
         for point in row:
             m, M = min(m, point), max(M, point)
@@ -32,14 +33,14 @@ def py_stats_i32(data):
     return Stats(m, M, total, mean, std)
 
 
-numba_stats_i32 = numba.njit()(py_stats_i32)
-numba_stats_i32.__name__ = "numba_stats_i32"
+numba_stats_int = numba.njit()(py_stats_int)
+numba_stats_int.__name__ = "numba_stats_int"
 
 
-def py_stats_i32_fast_std(data):
+def py_stats_int_fast_std(data):
     # with std in one loop (may be imprecise for big numbers)
     # (see https://www.strchr.com/standard_deviation_in_one_pass)
-    m, M, total, sq_total, N = MAX_I32, MIN_I32, 0, 0.0, data.size
+    m, M, total, sq_total, N = IMAX, IMIN, 0, 0.0, data.size
     for row in data:
         for point in row:
             m, M = min(m, point), max(M, point)
@@ -51,8 +52,8 @@ def py_stats_i32_fast_std(data):
     return Stats(m, M, total, mean, std)
 
 
-numba_stats_i32_fast_std = numba.njit()(py_stats_i32_fast_std)
-numba_stats_i32_fast_std.__name__ = "numba_stats_i32_fast_std"
+numba_stats_int_fast_std = numba.njit()(py_stats_int_fast_std)
+numba_stats_int_fast_std.__name__ = "numba_stats_int_fast_std"
 
 
 def test_stats():
@@ -64,17 +65,17 @@ def test_stats():
     )
 
     assert stats(data) == expected
-    assert py_stats_i32(data) == expected
-    assert numba_stats_i32(data) == expected
-    assert numba_stats_i32_fast_std(data) == expected
+    assert py_stats_int(data) == expected
+    assert numba_stats_int(data) == expected
+    assert numba_stats_int_fast_std(data) == expected
 
 
 def profile_stats():
     msg = "Warm up numba"
     print(f"{msg:.<50} ", end="", flush=True)
     d = numpy.array([[1,2],[3,4]], dtype='i4')
-    numba_stats_i32(d)
-    numba_stats_i32_fast_std(d)
+    numba_stats_int(d)
+    numba_stats_int_fast_std(d)
     print("DONE!")
 
     msg = "Running tests"
@@ -84,23 +85,29 @@ def profile_stats():
 
     from timeit import timeit
 
-    def profile(stats, shape, number):
+    def profile(stats, shape, dtype, number):
         size = numpy.multiply(*shape)
-        setup = f"import numpy; d = numpy.arange({size}, dtype='i4'); d.shape = {shape}"
+        setup = f"import numpy; d = numpy.arange({size}, dtype='{dtype}'); d.shape = {shape}"
         test = "stats(d)"
         namespace = dict(stats=stats)
-        msg = f"{stats.__name__} for {shape[0]}x{shape[1]}"
+        msg = f"{stats.__name__} for {shape[0]}x{shape[1]}x{dtype}"
         print(f"{msg:.<50} ", end="", flush=True)
         r = timeit(test, setup=setup, globals=namespace, number=number) / number
         print(f"{r*1000:.3f} ms per interaction")
 
     sizes = (1280, 720), (1920, 1080), (2560, 1440)
-    for size in sizes:
-        profile(py_stats_i32, size, 1)
-        profile(stats, size, 100)
-        profile(numba_stats_i32, size, 100)
-        profile(numba_stats_i32_fast_std, size, 100)
+    dtypes = 'i1', 'i2', 'i4', 'i8'
 
+    # Just for reference a pure python
+    profile(py_stats_int, (1280, 720), 'i4', 1)
+    print()
+    N = 100
+    for size in sizes:
+        for dtype in dtypes:
+            profile(stats, size, dtype, N)
+            profile(numba_stats_int, size, dtype, N)
+            profile(numba_stats_int_fast_std, size, dtype, N)
+            print()
 
 if __name__ == "__main__":
     profile_stats()
