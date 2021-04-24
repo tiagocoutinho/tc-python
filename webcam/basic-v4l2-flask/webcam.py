a0067d0e18d8d07835ede0a2a9f451039bfc7440
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2021-04-24 16:44:46 +0200

    Release V4L2 device when finished

--- webcam/basic-v4l2-flask/webcam.py
+++ webcam/basic-v4l2-flask/webcam.py
@@ -1,21 +1,23 @@
-import flask
 import select
-import v4l2capture
+import contextlib
 
+import flask
+import v4l2capture
 
 app = flask.Flask('basic-web-cam')
 
 
 def gen_frames():
     cam = v4l2capture.Video_device("/dev/video0")
-    cam.set_format(640, 480, fourcc='MJPG')
-    cam.create_buffers(1)
-    cam.queue_all_buffers()
-    cam.start()
-    while True:
-        select.select((cam,), (), ())
-        frame = cam.read_and_queue()
-        yield b"--frame\r\nContent-Type: image/jpeg\r\n\r\n" + frame + b"\r\n"
+    with contextlib.closing(cam):
+        cam.set_format(640, 480, fourcc='MJPG')
+        cam.create_buffers(1)
+        cam.queue_all_buffers()
+        cam.start()
+        while True:
+            select.select((cam,), (), ())
+            frame = cam.read_and_queue()
+            yield b"--frame\r\nContent-Type: image/jpeg\r\n\r\n" + frame + b"\r\n"
 
 
 @app.route("/")
