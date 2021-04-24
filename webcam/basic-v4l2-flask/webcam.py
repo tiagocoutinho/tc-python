493f56aac66b053f5eacdcfa22eb31ee3de3fed4
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2021-04-24 13:23:53 +0200

    Make code more readable

--- webcam/basic-v4l2-flask/webcam.py
+++ webcam/basic-v4l2-flask/webcam.py
@@ -5,6 +5,7 @@ import v4l2capture
 
 app = flask.Flask('basic-web-cam')
 
+
 def gen_frames():
     cam = v4l2capture.Video_device("/dev/video0")
     cam.set_format(640, 480, fourcc='MJPG')
@@ -17,11 +18,16 @@ def gen_frames():
         yield b"--frame\r\nContent-Type: image/jpeg\r\n\r\n" + frame + b"\r\n"
 
 
-app.route("/")(lambda: '<!DOCTYPE html><html><body><img src="/stream" /></body></html>')
+@app.route("/")
+def index():
+    return '<!DOCTYPE html><html><body><img src="/stream" /></body></html>'
+
+
 @app.route("/stream")
 def stream():
     return flask.Response(
         gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
