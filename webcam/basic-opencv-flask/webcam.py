4b77428a74035f383f516dfc5e1929f84245f5f9
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2021-04-24 13:23:53 +0200

    Make code more readable

--- webcam/basic-opencv-flask/webcam.py
+++ webcam/basic-opencv-flask/webcam.py
@@ -1,7 +1,9 @@
 import cv2
 import flask
 
+
 app = flask.Flask('basic-web-cam')
+
 
 def gen_frames():
     cam = cv2.VideoCapture(0)
@@ -10,11 +12,17 @@ def gen_frames():
         _, data = cv2.imencode('.jpg', frame)
         yield b"--frame\r\nContent-Type: image/jpeg\r\n\r\n" + data.tobytes() + b"\r\n"
 
-app.route("/")(lambda: '<!DOCTYPE html><html><body><img src="/stream" /></body></html>')
+
+@app.route("/")
+def index():
+    return '<!DOCTYPE html><html><body><img src="/stream" /></body></html>'
+
+
 @app.route("/stream")
 def stream():
     return flask.Response(
         gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
