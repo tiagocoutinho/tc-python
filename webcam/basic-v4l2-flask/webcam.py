2251f99c40561e7003d63d4651226c6dabb73d0a
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2021-04-24 13:21:39 +0200

    Use directly MJPEG format

--- webcam/basic-v4l2-flask/webcam.py
+++ webcam/basic-v4l2-flask/webcam.py
@@ -1,27 +1,20 @@
-import io
-import select
-import PIL.Image
 import flask
+import select
 import v4l2capture
+
 
 app = flask.Flask('basic-web-cam')
 
 def gen_frames():
     cam = v4l2capture.Video_device("/dev/video0")
-    cam.set_format(640, 480)
+    cam.set_format(640, 480, fourcc='MJPG')
     cam.create_buffers(1)
     cam.queue_all_buffers()
-    buff = io.BytesIO()
     cam.start()
     while True:
         select.select((cam,), (), ())
         frame = cam.read_and_queue()
-        image = PIL.Image.frombytes("RGB", (640, 480), frame)
-        image.save(buff, format="jpeg")
-        data = buff.getvalue()
-        yield b"--frame\r\nContent-Type: image/jpeg\r\n\r\n" + data + b"\r\n"
-        buff.seek(0)
-        buff.truncate()
+        yield b"--frame\r\nContent-Type: image/jpeg\r\n\r\n" + frame + b"\r\n"
 
 
 app.route("/")(lambda: '<!DOCTYPE html><html><body><img src="/stream" /></body></html>')
