88fe14d099d34c21c3d89d1967399c03ec5fe2b9
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2022-06-26 18:26:01 +0200

    replace concurrent.futures ThreadPoolExecutor with multiprocessing ThreadPool

--- multiprocessing/ports/prefork/server.py
+++ multiprocessing/ports/prefork/server.py
@@ -3,19 +3,20 @@
 import socket
 import logging
 import multiprocessing.pool
-from concurrent.futures import ThreadPoolExecutor
 
 import click
 
 
-log = logging.getLogger("server")
 mlog = logging.getLogger("master")
-wlog = log.getChild("worker")
+wlog = logging.getLogger("worker")
 
 
 def config(log_level):
-    fmt = "%(asctime)s %(levelname)s %(processName)s.%(threadName)s %(name)s %(message)s"
-    logging.basicConfig(level=log_level.upper(), format=fmt)
+    fmt = "%(asctime)s %(levelname)-5s %(processName)s/%(threadName)s/%(name)s %(message)s"
+    logging.basicConfig(level=log_level, format=fmt)
+
+    #logger = multiprocessing.get_logger()
+    #logger.propagate = 1
 
 
 def address(addr):
@@ -39,17 +40,24 @@ def handle_client(sock, addr):
                     return
                 wlog.debug("req: %r", data)
                 reply = data[::-1]
-                #time.sleep(0.001)
                 sock.sendall(reply)
                 wlog.debug("reply: %r", reply)
         except OSError as error:
             wlog.info("socket error from %r: %r", addr, error)
 
 
-def worker_loop(server):
+def worker_loop(server, handler):
     wlog.info("ready to accept requests")
-    while True:
-        handle_client(*server.accept())
+    try:
+        while True:
+            client, addr = server.accept()
+            handler(client, addr)
+    finally:
+        wlog.info("thread graceful shutdown")
+
+
+def worker_init():
+    wlog.info("starting new worker thread...")
 
 
 def worker_main(server, threads, log_level):
@@ -57,40 +65,35 @@ def worker_main(server, threads, log_level):
     wlog.info("starting worker...")
     try:
         with server:
-            if threads == -1:
-                worker_loop(server)
+            if threads:
+                with multiprocessing.pool.ThreadPool(threads, worker_init) as pool:
+                    def handler(*args):
+                        pool.apply_async(handle_client, args)
+                    worker_loop(server, handler)
             else:
-                with ThreadPoolExecutor(max_workers=threads, thread_name_prefix="TH") as exe:
-                    for _ in range(threads):
-                        exe.submit(worker_loop, server)
+                worker_loop(server, handle_client)
+
 
     except Exception as error:
-        log.info("unhandled error: %r", error)
+        wlog.info("unhandled error: %r", error)
     except KeyboardInterrupt:
-        log.info("ctrl-C pressed. Bailing out")
+        wlog.info("ctrl-C pressed. Bailing out")
     finally:
-        try:
-            log.info("start worker graceful shutdown...")
-            log.info("finished worker graceful shutdown")
-        except BaseException as error:
-            log.error("error in shutdown: %r", error)
+        wlog.info("process graceful shutdown")
 
 
 def master_main(addr, log_level, nb_threads):
-    log = logging.getLogger("master")
     server = Server(addr)
     with server:
         try:
             worker_main(server, nb_threads, log_level)
         except KeyboardInterrupt:
-            log.info("ctrl-C pressed. Bailing out")
+            mlog.info("ctrl-C pressed. Bailing out")
         finally:
-            log.info("start master graceful shutdown!")
-            log.info("finished master graceful shutdown")
+            mlog.info("master graceful shutdown")
 
 
 def master_pre_fork(addr, log_level, nb_processes, nb_threads):
-    log = logging.getLogger("master")
     ctx = multiprocessing.get_context("fork")
     server = Server(addr)
     pool = ctx.Pool(processes=nb_processes)
@@ -102,18 +105,15 @@ def master_pre_fork(addr, log_level, nb_processes, nb_threads):
 
 @click.command()
 @click.option('--listen', "addr", default=':5000', type=address, show_default=True)
-@click.option('--log-level', default='INFO', show_default=True)
-@click.option('-p', '--processes', "nb_processes", default=-1, show_default=True)
-@click.option('-t', '--threads', "nb_threads", default=-1, show_default=True)
+@click.option('--log-level', default='INFO', type=str.upper, show_default=True)
+@click.option('-p', '--processes', "nb_processes", default=0, show_default=True)
+@click.option('-t', '--threads', "nb_threads", default=0, show_default=True)
 def main(addr, log_level, nb_processes, nb_threads):
     config(log_level)
-    log = logging.getLogger("master")
-    log.info("start listening to %r", addr)
-
-    if nb_processes == -1:
-        master_main(addr, log_level, nb_threads)
-    else:
+    if nb_processes:
         master_pre_fork(addr, log_level, nb_processes, nb_threads)
+    else:
+        master_main(addr, log_level, nb_threads)
 
 
 if __name__ == "__main__":
