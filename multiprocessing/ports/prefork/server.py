c3d7e95dd6494a173dd7e79a6a98d523f864441d
Jose Tiago Macara Coutinho <coutinhotiago@gmail.com>
2022-07-01 20:04:51 +0200

    Use Process and Thread directly (need to manage resources carefully)

--- multiprocessing/ports/prefork/server.py
+++ multiprocessing/ports/prefork/server.py
@@ -1,7 +1,9 @@
 """Simple socket with pre-fork model"""
 
+import select
 import socket
 import logging
+import threading
 import multiprocessing.pool
 
 import click
@@ -46,43 +48,66 @@ def handle_client(sock, addr):
             wlog.info("socket error from %r: %r", addr, error)
 
 
-def worker_loop(server, handler):
+def worker_loop(server, channel):
     wlog.info("ready to accept requests")
     try:
-        while True:
-            client, addr = server.accept()
-            handler(client, addr)
+        with server, channel:
+            while True:
+                socks, _, _ = select.select((server, channel), (), ())
+                for sock in socks:
+                    if sock is channel:
+                        wlog.info("received stop signal")
+                        assert channel.recv(1024) == b"STOP\n"
+                        return
+                    elif sock is server:
+                        try:
+                            client, addr = server.accept()
+                            handle_client(client, addr)
+                        except OSError as error:
+                            wlog.warning("accept error: %r", error)
+                            return
     finally:
         wlog.info("thread graceful shutdown")
 
 
-def worker_init():
-    wlog.info("starting new worker thread...")
-
-
 def worker_main(server, threads, log_level):
     config(log_level)
-    wlog.info("starting worker...")
+    wlog.info("starting worker process...")
     try:
         with server:
             if threads:
-                with multiprocessing.pool.ThreadPool(threads, worker_init) as pool:
-                    def handler(*args):
-                        pool.apply_async(handle_client, args)
-                    worker_loop(server, handler)
+                channels = []
+                for i in range(threads):
+                    s1, s2 = socket.socketpair()
+                    args = server, s2
+                    thread = threading.Thread(
+                        target=worker_loop,
+                        args=args,
+                        name=f"WorkerThread-{i:02d}"
+                    )
+                    channels.append((thread, s1))
+                for thread, _ in channels:
+                    thread.start()
+                for thread, _ in channels:
+                    thread.join()
             else:
-                worker_loop(server, handle_client)
+                worker_loop(server, None)
     except Exception as error:
         wlog.info("unhandled error: %r", error)
     except KeyboardInterrupt:
-        wlog.info("ctrl-C pressed. Bailing out")
+        wlog.info("Ctrl-C pressed. Bailing out")
     finally:
-        wlog.info("process graceful shutdown")
+        wlog.info("start worker process graceful shutdown")
+        server.close()
+        if threads:
+            for _, channel in channels:
+                channel.sendall(b"STOP\n")
+            for thread, _ in channels:
+                thread.join()
 
 
 def master_main(addr, reuse_port, log_level, nb_threads):
-    server = Server(addr, reuse_port=reuse_port)
-    with server:
+    with Server(addr, reuse_port=reuse_port) as server:
         try:
             worker_main(server, nb_threads, log_level)
         except KeyboardInterrupt:
@@ -92,13 +117,24 @@ def master_main(addr, reuse_port, log_level, nb_threads):
 
 
 def master_pre_fork(addr, reuse_port, log_level, nb_processes, nb_threads):
-    ctx = multiprocessing.get_context("fork")
-    server = Server(addr, reuse_port=reuse_port)
-    pool = ctx.Pool(processes=nb_processes)
-    with server, pool:
-        args = server, nb_threads, log_level
-        result = pool.starmap_async(worker_main, nb_processes*[args])
-        result.wait()
+    workers = []
+    try:
+        with Server(addr, reuse_port=reuse_port) as server:
+            ctx = multiprocessing.get_context("fork")
+            args = server, nb_threads, log_level
+            workers = [
+                ctx.Process(target=worker_main, args=args, name=f"WorkerProcess-{i:02d}")
+                for i in range(nb_processes)
+            ]
+
+            [worker.start() for worker in workers]
+            [worker.join() for worker in workers]
+    except KeyboardInterrupt:
+        mlog.info("Ctrl-C pressed. Bailing out")
+    finally:
+        mlog.info("start master graceful shutdown...")
+        [worker.join() for worker in workers]
+        mlog.info("finished master graceful shutdown")
 
 
 @click.command()
