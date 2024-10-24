d91674b1646fdfe4ac4b085300d15365e4aa3fd1
Jose Tiago Macara Coutinho <tiago.coutinho@crunch.io>
2024-10-24 06:59:21 +0200

    html generator v1

---
+++ htmx/ht.py
@@ -0,0 +1,53 @@
+
+
+
+class Tag:
+    def __init__(self, name):
+        self.name = name
+    
+    def render(self, *children, **options):
+        children = "".join(children)
+        options = " ".join(
+            f'{name}="{value}"' for name, value in options.items()
+        )
+        return f"<{self.name} {options}>{children}</{self.name}>"
+
+    __call__ = render
+
+
+
+
+def noop(*args, **kwargs): return ""
+
+TAG_NAMES = [
+    "a", "abbr", "address", "area", "article", "aside", "audio", "b", "base", "bdi", "bdo",
+    "blockquote", "body", "br", "button", "canvas", "caption", "cite", "code", "col", "colgroup",
+    "data", "datalist", "dd", "del", "details", "dfn", "dialog", "div", "dl", "dt", "em", "embed",
+    "fieldset", "figcaption", "figure", "footer", "form", "h1", "h2", "h3", "h4", "h5", "h6",
+    "head", "header", "hr", "html", "i", "iframe", "img", "input", "ins", "kbd", "label", "legend",
+    "li", "link", "main", "map", "mark", "meta", "meter", "nav", "noscript", "object", "ol", "optgroup",
+    "option", "output", "p", "param", "picture", "pre", "progress", "q", "rp", "rt", "ruby", "s", "samp",
+    "script", "section", "select", "small", "source", "span", "strong", "style", "sub", "summary", "sup",
+    "svg", "table", "tbody", "td", "template", "textarea", "tfoot", "th", "thead", "time", "title", "tr",
+    "track", "u", "ul", "var", "video", "wbr"
+]
+
+def _fill_tags(d):
+    for name in TAG_NAMES:
+        name = name.upper()
+        d[name] = Tag(name)
+
+_fill_tags(locals())
+
+def main():
+    html = HTML(
+        HEAD(
+            TITLE("Hello!"),
+            SCRIPT(src="/bla"),
+            LINK(href="", rel="")
+        )
+    )
+    print(html)
+
+if __name__ == "__main__":
+    main()
