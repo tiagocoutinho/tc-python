c4bebeb9005aeabacba2b05b3e25f31ca619f567
Jose Tiago Macara Coutinho <tiago.coutinho@crunch.io>
2024-10-24 10:59:13 +0200

    html generator v4

--- htmx/ht.py
+++ htmx/ht.py
@@ -9,10 +9,12 @@ def render_attr(key: str, value):
         return ""
     return f'{key}="{value}"'
 
+
 def render_attrs(options):
     return " ".join(render_attr(k, v) for k, v in options.items())
 
-def render(tag: str, children: Iterable[str], attrs) -> str:
+
+def render_simple(tag: str, children: Iterable[str], attrs) -> str:
     children = "".join(children)
     attrs = render_attrs(attrs)
     if children:
@@ -20,29 +22,43 @@ def render(tag: str, children: Iterable[str], attrs) -> str:
         return f"{start}{children}</{tag}>"
     return f"<{tag} {attrs}/>"
 
+
 class Tag:
     def __init__(self, name):
         self.name = name
     
     def render(self, *children, **options):
-        return render(self.name, children, options)
+        return render_simple(self.name, children, options)
 
     __call__ = render
 
 
+def pretty_render(elem, level=0, indent="  ", endl="\n"):
+    prefix = level * indent
+    if isinstance(elem, str):
+        return f"{prefix}{elem}"
+    tag = elem.tag
+    attrs = render_attrs(elem.attrs)
+    children = elem.children
+    if children:
+        start = f"{prefix}<{tag} {attrs}>" if attrs else f"{prefix}<{tag}>"
+        if len(children) == 1 and isinstance(children[0], str):
+            return f"{start}{children[0]}</{tag}>"
+        children = endl.join(pretty_render(child, level+1, indent, endl) for child in children)
+        return f"{start}{endl}{children}{endl}{prefix}</{tag}>"
+    return f"{prefix}<{tag} {attrs}/>"
+
+
 class Element:
+    tag = "None"
+    __slots__ = ["children", "attrs"]
+
     def __init__(self, *children, **attrs):
         self.children = children
         self.attrs = attrs
-    
-    @staticmethod
-    def render_child(c):
-        return c if isinstance(c, str) else c.render()
 
-    def render(self):
-        children = (self.render_child(child) for child in self.children)
-        tag = type(self).__name__
-        return render(tag, children, self.attrs)
+    def __str__(self):
+        return render_simple(self.tag, (str(child) for child in self.children), self.attrs)
 
 
 TAG_NAMES = [
@@ -60,8 +76,9 @@ TAG_NAMES = [
 
 def _fill_tags(d):
     for name in TAG_NAMES:
-        d[name] = types.new_class(name, (Element,))
-
+        klass = types.new_class(name, (Element,))
+        klass.tag = name.upper()
+        d[name] = klass
         name = name.upper()
         d[name] = Tag(name)
 
@@ -80,16 +97,31 @@ def main():
     print(html)
 
 
-def main2():
-    doc = html(
+def sample():
+    return html(
         head(
-            title("Hello!"),
-            script(src="/bla"),
-            link(href="", rel="", defer=True)
+            title('Sample Page'),
+            link(rel='stylesheet', href='style.css'),
+            script(src="https://unpkg.com/htmx.org@2.0.3", defer=True)
+        ),
+        body(
+            h1('Welcome to My Website'),
+            p('This is a paragraph.'),
+            div(
+                p('Another paragraph inside a div.'),
+                id='content',
+            )
         )
     )
-    print(doc.render())
+
+def main2(level=0, indent="  ", end="\n"):
+    print(80*"=")
+    doc = sample()
+    print(doc)
+    print(80*"=")
+    print(pretty_render(doc, level, indent, end))
+    print(80*"=")
 
 
 if __name__ == "__main__":
-    main()
+    main2()
