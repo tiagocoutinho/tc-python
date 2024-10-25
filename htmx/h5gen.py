3129eee6a37377a01a5ec8d6c86b99a215413835
Jose Tiago Macara Coutinho <tiago.coutinho@crunch.io>
2024-10-25 12:28:52 +0200

    Remove render_simple

--- htmx/h5gen.py
+++ htmx/h5gen.py
@@ -125,15 +125,7 @@ def render_attrs(options):
     return " ".join(render_attr(k, v) for k, v in options.items() if v is not False)
 
 
-def render_simple(tag, children, attrs) -> str:
-    children = "".join(children)
-    attrs = render_attrs(attrs)
-    start = f"<{tag} {attrs}>" if attrs else f"<{tag}>"
-    end = "" if tag in NO_END_TAG else f"</{tag}>"
-    return f"{start}{children}{end}"
-
-
-def iter_render_pretty(elem, level=0, indent="  ", endl="\n"):
+def iter_render(elem, level=0, indent="  ", endl="\n"):
     prefix = level * indent
     if isinstance(elem, str):
         yield f"{prefix}{elem}{endl}"
@@ -153,13 +145,13 @@ def iter_render_pretty(elem, level=0, indent="  ", endl="\n"):
         return
     yield f"{start}{endl}"
     for child in children:
-        yield from iter_render_pretty(child, level + 1, indent, endl)
+        yield from iter_render(child, level + 1, indent, endl)
     yield f"{prefix}{etag}{endl}"
 
 
-def render_pretty(elem, level=0, indent="  ", endl="\n"):
+def render(elem, level=0, indent="  ", endl="\n"):
     return f"{elem.prefix}{endl}" + "".join(
-        iter_render_pretty(elem, level=level, indent=indent, endl=endl)
+        iter_render(elem, level=level, indent=indent, endl=endl)
     )
 
 
@@ -178,9 +170,7 @@ class Element:
         self.attrs = py_dict_to_attrs(attrs)
 
     def __str__(self):
-        return self.prefix + render_simple(
-            self.tag, (str(child) for child in self.children), self.attrs
-        )
+        return render(self, 0, "", "")
 
     def __getitem__(self, key):
         if isinstance(key, tuple):
@@ -190,14 +180,16 @@ class Element:
         return self.attrs[key]
 
 
-def _fill_tags(d):
+def _fill_tags():
+    result = {}
     for name in TAG_NAMES:
         klass = types.new_class(name, (Element,))
         klass.tag = name
-        d[name.capitalize()] = klass
+        result[name.capitalize()] = klass
+    return result
 
 
-_fill_tags(locals())
+locals().update(_fill_tags())
 
 Html.prefix = "<!doctype html>"
 
