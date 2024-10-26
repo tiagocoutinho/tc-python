00a79a583e690328e4bbb371ea169f03e401f3d3
Jose Tiago Macara Coutinho <tiago.coutinho@crunch.io>
2024-10-26 06:03:37 +0200

    Add helpers

--- htmx/h5gen.py
+++ htmx/h5gen.py
@@ -193,5 +193,112 @@ locals().update(_fill_tags())
 
 Html.prefix = "<!doctype html>"
 
+Checkbox = functools.partial(Input, type="checkbox")
 
-## H
+## Favicon
+
+ICON_NULL = "data:;base64,iVBORw0KGgo="
+
+
+def Favicon(href, type="image/x-icon", **kwargs):
+    return Link(rel="icon", href=href, type=type, **kwargs)
+
+
+favicon_null = Favicon("data:;base64,iVBORw0KGgo=")
+
+## Metas
+
+Viewport = functools.partial(Meta, name="viewport")
+
+utf_8 = Meta(charset="utf-8")
+viewport = Viewport(content="width=device-width, initial-scale=1")
+
+## Javascript
+
+JS = functools.partial(Script, type="text/javascript")
+
+## HTMX
+
+HTMX_UNPKG_SOURCE = "https://unpkg.com/htmx.org@2.0.3/dist/htmx.min.js"
+HTMX_UNPKG_EXT_TEMPLATE = "https://unpkg.com/htmx-ext-{name}@{version}/{name}.js".format
+
+HTMX_JSDELIVR_SOURCE = "https://cdn.jsdelivr.net/npm/htmx.org@2.0.3/dist/htmx.min.js"
+HTMX_JSDELIVR_EXT_TEMPLATE = (
+    "https://cdn.jsdelivr.net/npm/htmx-ext-{name}@{version}/{name}.js".format
+)
+
+HTMX_EXTENSIONS = {
+    "preload": {
+        "url": HTMX_JSDELIVR_EXT_TEMPLATE(name="preload", version="2.0.1"),
+        "ext_name": "preload",
+    },
+}
+
+
+def HtmxScript(defer=True, **kwargs):
+    return Script(src=HTMX_JSDELIVR_SOURCE, defer=defer, **kwargs)
+
+
+def HtmxExtension(name, defer=True, **kwargs):
+    return Script(src=HTMX_EXTENSIONS[name]["url"], defer=defer, **kwargs)
+
+
+def Htmx(
+    body, title="h5gen page", icon=ICON_NULL, extra_headers=(), extensions=(), lang="en"
+):
+    """Helper that creates an HTML document with prepared HTMX headers
+
+    Args:
+        body (Body): an instance of Body
+        title (str, optional): Page title. Defaults to "h5gen page".
+        icon (_type_, optional): page fav icon. Defaults to null icon.
+        extra_headers (tuple, optional): _description_. Defaults to ().
+        extensions (tuple, optional): _description_. Defaults to ().
+
+    Returns:
+        _type_: _description_
+    """
+    scripts = [HtmxScript()]
+    if extensions:
+        hx_exts = {
+            name.strip()
+            for name in body.attrs.get("hx-ext", "").split(",")
+            if name.strip()
+        }
+        for extension in extensions:
+            ext = HTMX_EXTENSIONS[extension]
+            hx_exts.add(ext["ext_name"])
+            scripts.append(HtmxExtension(extension))
+        print(hx_exts)
+        body.attrs["hx-ext"] = ",".join(hx_exts)
+    return Html(
+        Head(
+            Title(title),
+            Favicon(icon),
+            utf_8,
+            viewport,
+            *scripts,
+            *extra_headers,
+        ),
+        body,
+        lang=lang,
+    )
+
+
+def Css(href, **kwargs):
+    return Link(rel="stylesheet", href=href, **kwargs)
+
+
+# Pico CSS
+
+PICOCSS_SOURCE = "https://cdn.jsdelivr.net/npm/@picocss/pico@2.0.6/css/pico.min.css"
+
+
+pico_css = Css(href=PICOCSS_SOURCE)
+
+pico_css_style = Style("""
+button {
+    --pico-form-element-spacing-horizontal: 0.5rem;
+    --pico-form-element-spacing-vertical: 0.2rem;
+}
+""")
